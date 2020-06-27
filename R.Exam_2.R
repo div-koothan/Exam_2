e9701bbd021c1a8d82f8b0dc5c4d3a14b487498e
Divya Koothan <div.koothan@utexas.edu>
2020-06-26 19:22:09 -0500

    completed up to Q.10

--- R.Exam_2.R
+++ R.Exam_2.R
@@ -10,3 +10,30 @@ inequality_data <- import("inequality.xlsx")
 
 #viewing data set to determine cross sectional or not 
 summary(inequality_data)
+
+
+#subset Gini index for Sweden and Denmark
+subset(inequality_data, select = c("country", "inequality_gini"), country == "Sweden"|country == "Denmark")
+
+#subset gingi index for Brazil
+subset(inequality_data, select = c("country", "inequality_gini"), country == "Brazil")
+
+#peak at dataframe
+head(inequality_data)
+
+#create function
+remove.accents <- function(s){
+  #1 character substitutions
+  old1 <- "ú"
+  new1 <- "u"
+  s1  <-chartr(old1,new1, s)
+  
+  #2 character substitutions 
+  old2 <- c("ß")
+  new2 <- c("ss")
+  s2 <- s1
+  for (i in seq_along(old2))
+    s2 <-gsub(old2[1], new2[i], s2, fixed = TRUE )
+  
+  s2
+}
